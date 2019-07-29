4bf1dd6396f261bf52fd262eae1bfe95909a6d46
Zaineb <zenp12@gmail.com>
2019-07-29 21:27:18 +0530

    handle

---
+++ twitter_handle.py
@@ -0,0 +1,19 @@
+import json
+import requests
+from requests_oauthlib import OAuth1
+
+api_key = 'Z0P4OidK4cKttEmzDdQRSckpa' # enter api key
+api_secret_key = 'rqpAf5m3pbnoanYR1Z5BLYFkksiuxhwOQzhv8PeVmHsa7rXySR' # enter api secret key
+auth = OAuth1(api_key, api_secret_key)
+
+screen_name="thevirdas"
+url="https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+screen_name+"+&count=200"
+
+ans=requests.get(url,auth=auth)
+j_ans=json.loads(ans.text)
+j_ans
+
+for text in j_ans:
+    print(text["text"])
+    print("\n")
+   
